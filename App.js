8d862f9dff9f6fd635b1936b478ff61300f80ad9
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-02-16 03:11:01 +0100

    feat: making a bar code scanner with expo

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
+import { BarCodeScanner } from 'expo-barcode-scanner';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [hashPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hashPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+
+  if (hashPermission === false) {
+    return <Text>No acess to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && (
+        <Button title={'Tap to scan Again'} onPress={() => setScanned(false)} />
+      )}
       <StatusBar style="auto" />
     </View>
   );
